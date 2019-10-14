beae6017bdd0439999f583bf09f407cc28421a42
unknown <affankhan627@gmail.com>
2019-10-15 04:36:29 +0500

    seed

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -124,11 +124,11 @@ public:
         assert(genesis.hashMerkleRoot == uint256S("0x059295a016a26a33377c50390b886c0a06712f910dcad02a140bf03344230498"));
 
         // Note that of those with the service bits flag, most only support a subset of possible options
-        vSeeds.emplace_back("seed-a.testcoin.loshan.co.uk", true);
-        vSeeds.emplace_back("dnsseed.thrasher.io", true);
-        vSeeds.emplace_back("dnsseed.testcointools.com", true);
-        vSeeds.emplace_back("dnsseed.testcoinpool.org", true);
-        vSeeds.emplace_back("dnsseed.koin-project.com", false);
+        // vSeeds.emplace_back("seed-a.testcoin.loshan.co.uk", true);
+        // vSeeds.emplace_back("dnsseed.thrasher.io", true);
+        // vSeeds.emplace_back("dnsseed.testcointools.com", true);
+        // vSeeds.emplace_back("dnsseed.testcoinpool.org", true);
+        // vSeeds.emplace_back("dnsseed.koin-project.com", false);
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,63);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
